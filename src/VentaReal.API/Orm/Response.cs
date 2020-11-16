02556b2417cb209b8cedbbdc63ec13acc157b4f0
Pedro Ruiz <pedrormiguel@outlook.es>
2020-11-16 09:55:44 -0400

    Se creó contructor de la clase Response.

--- src/VentaReal.API/Orm/Response.cs
+++ src/VentaReal.API/Orm/Response.cs
@@ -5,6 +5,11 @@ namespace VentaReal.API.Orm
 {
     public class Response {
 
+        public Response()
+        {
+            data   = null;
+            exito  = 0;
+        }
         public string message { get; set; }
         public double exito { get; set; }
         public Object data { get; set; }
