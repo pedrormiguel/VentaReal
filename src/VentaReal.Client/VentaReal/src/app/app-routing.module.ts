c462f6e6b3f040772984470f30b3a8aef2652059
Pedro Ruiz <pedrormiguel@outlook.es>
2020-11-16 09:58:54 -0400

    Creamos Guard y interceptors para las rutas.

--- src/VentaReal.Client/VentaReal/src/app/app-routing.module.ts
+++ src/VentaReal.Client/VentaReal/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { Authen } from './security/Auth/auth.guard';
 
 const routes: Routes = [
 
   { path: '', redirectTo:  '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent, canActivate: [Authen] },
+  { path: 'login', component: LoginComponent}
 
 ];
 
--- src/VentaReal.Client/VentaReal/src/app/app.module.ts
+++ src/VentaReal.Client/VentaReal/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';  
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CarComponent } from './car/car.component';
+import { DialogComponent } from './common/dialog/dialog.component';
 
 
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -18,19 +19,19 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
-
-import { DialogComponent } from './common/dialog/dialog.component';
-
-
-
+import { Authen } from './security/Auth/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { JWTInterceptor } from './services/jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CarComponent,
-    DialogComponent
+    DialogComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -43,11 +44,16 @@ import { DialogComponent } from './common/dialog/dialog.component';
     MatButtonModule,
     MatSnackBarModule,
     MatCardModule,
+    MatInputModule,
 
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ 
+    Authen,
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
